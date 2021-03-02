28550a6bcb3128c7674ec0e5cca2d036d92b7603
paulwooolf <paulwooolf@gmail.com>
2021-03-03 00:43:19 +0300

    97: hooks

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
+    return (
+        <div>
+            <HookSwitcher />
+        </div>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const HookSwitcher = () => {
+
+    const [color, setColor ] = useState('white');
+    const [fontSize, setFontSize] = useState(14);
+
+    return (
+        <div style={{ padding: '10px',
+            backgroundColor: color,
+            fontSize: `${fontSize}px`
+        }}>
+            <p>Hello, world</p>
+            <button onClick={() => setColor('black')}>Dark</button>
+            <button onClick={() => setColor('white')}>Light</button>
+            <button onClick={() => setFontSize((s) => s+2)}>+</button>
+            <button onClick={() => setFontSize((s) => s-2)}>-</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
