7991d3e4e0d72df88e96cd798d9b6fd0a9d13e32
paulwooolf <paulwooolf@gmail.com>
2021-03-03 00:51:04 +0300

    98: hooks

--- src/index.js
+++ src/index.js
@@ -1,31 +1,19 @@
-import React, { useState } from 'react'
-import ReactDOM from 'react-dom'
+import React, { useContext } from "react";
+import ReactDOM from 'react-dom';
+
+const MyContext = React.createContext();
 
 const App = () => {
     return (
-        <div>
-            <HookSwitcher />
-        </div>
+        <MyContext.Provider value="Hello world 123">
+            <Child />
+        </MyContext.Provider>
     )
 }
 
-const HookSwitcher = () => {
-
-    const [color, setColor ] = useState('white');
-    const [fontSize, setFontSize] = useState(14);
-
-    return (
-        <div style={{ padding: '10px',
-            backgroundColor: color,
-            fontSize: `${fontSize}px`
-        }}>
-            <p>Hello, world</p>
-            <button onClick={() => setColor('black')}>Dark</button>
-            <button onClick={() => setColor('white')}>Light</button>
-            <button onClick={() => setFontSize((s) => s+2)}>+</button>
-            <button onClick={() => setFontSize((s) => s-2)}>-</button>
-        </div>
-    )
+const Child = () => {
+    const value = useContext(MyContext);
+    return <p>{value}</p>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/use-state.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+    return (
+        <div>
+            <HookSwitcher />
+        </div>
+    )
+}
+
+const HookSwitcher = () => {
+
+    const [color, setColor ] = useState('white');
+    const [fontSize, setFontSize] = useState(14);
+
+    return (
+        <div style={{ padding: '10px',
+            backgroundColor: color,
+            fontSize: `${fontSize}px`
+        }}>
+            <p>Hello, world</p>
+            <button onClick={() => setColor('black')}>Dark</button>
+            <button onClick={() => setColor('white')}>Light</button>
+            <button onClick={() => setFontSize((s) => s+2)}>+</button>
+            <button onClick={() => setFontSize((s) => s-2)}>-</button>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
